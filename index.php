0496efb5529fda0e801b6b1421b64546991a6365
gitName <bartk@list.ru>
2025-03-27 20:58:17 +0300

    feat: add the login form

--- index.php
+++ index.php
@@ -20,10 +20,30 @@
         integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy"
         crossorigin="anonymous"></script>
 
-        <link rel="stylesheet" href="static/style/style.css">
+    <link rel="stylesheet" href="static/style/style.css">
 </head>
 
-<body>
+<body class="vh-100">
+    <div class="container h-100 p-5">
+        <div class="d-flex flex-column  justify-content-center align-items-center h-100">
+            <h1 class="text-center">Log in</h1>
+            <div class="w-400 h-100 d-flex align-items-center">
+                <form class="w-100 p-5 rounded shadow">
+                    <div class="mb-3">
+                        <label for="nameFormInput" class="form-label">User name</label>
+                        <input type="text" class="form-control" id="nameFormInput">
+                    </div>
+                    <div class="mb-3">
+                        <label for="passwordFormInput" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="passwordFormInput">
+                    </div>
+                    <button type="submit" class="btn btn-primary">login</button>
+                    <small>New user?</small>
+                    <a href="signup.php">Registration</a>
+                </form>
+            </div>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"

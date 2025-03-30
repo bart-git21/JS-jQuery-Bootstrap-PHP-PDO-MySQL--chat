e32d2a637886e597806b72e6d83a37bc86b151ec
gitName <bartk@list.ru>
2025-03-30 23:09:38 +0300

    chore: destroy session cookie if no logged user

--- components/layouts/header.php
+++ components/layouts/header.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="static/images/logo.png" alt=""></a>
+        <a class="navbar-brand" href="#"><img src="/static/images/logo.png" alt=""></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -8,7 +8,7 @@
         <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="">Home</a>
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
                 </li>
             </ul>
 
@@ -17,15 +17,57 @@
                 session_start();
                 if (isset($_SESSION['userName'])) {
                     $name = $_SESSION['userName'];
-                    echo "<div class='me-3 d-flex align-items-center notSelected'><img class='me-2 h-30' src='static/images/person.png'>$name</div><button type='button' class='btn btn-outline-info'>Log out</button>";
-                } else { ?>
-                    <a class="nav-link active" aria-current="page" href="pages/login.php">log
-                        in</a>
+                    echo "<div class='me-3 d-flex align-items-center notSelected'><img class='me-2 h-30' src='/static/images/person.png'>$name</div><button id='logoutBtn' type='button' class='btn btn-outline-info'>Log out</button>";
+                } else {
+                    if (ini_get("session.use_cookies")) {
+                        $params = session_get_cookie_params();
+                        setcookie(
+                            session_name(),
+                            '',
+                            time() - 42000,
+                            $params["path"],
+                            $params["domain"],
+                            $params["secure"],
+                            $params["httponly"]
+                        );
+                    }
+                    session_destroy();
+                    ?>
+                    <a class="nav-link active" aria-current="page" href="/pages/login.php"><button
+                            type="button" class="btn btn-outline-primary">log in</button>
+                    </a>
                 <?php } ?>
             </form>
         </div>
     </div>
 </nav>
+
+<script>
+    $(document).ready(function () {
+        $('#logoutBtn').click(function (event) {
+            event.preventDefault();
+            const data = {
+                userId: localStorage.getItem('userId')
+            }
+            $.ajax({
+                url: "/api/auth",
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                data: JSON.stringify(data),
+            })
+                .done(response => {
+                    localStorage.removeItem('userId');
+                    location.reload();
+                })
+                .fail((xhr, status, error) => {
+                    console.log(xhr.status);
+                    xhr.responseJSON.error && console.log(xhr.responseJSON.error);
+                })
+        })
+    })
+</script>
 
 <style>
     .navbar-brand img {
@@ -35,7 +77,7 @@
     .h-30 {
         height: 30px;
     }
-    
+
     .notSelected {
         user-select: none;
     }
